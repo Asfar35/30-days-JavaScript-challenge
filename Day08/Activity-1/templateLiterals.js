8e057da30fc779663ec61c68490d41855b9c6375
Sk Asfar Ali <116512658+Asfar35@users.noreply.github.com>
2024-07-20 22:31:25 +0530

    Add files via upload

---
+++ Day08/Activity-1/templateLiterals.js
@@ -0,0 +1,12 @@
+//Activity-1: Template Literals
+//Task-1
+let name = "Jonny",
+    age = 20;
+let str = `Hiii ${name}.Happy ${age}...`;
+console.log(str);
+
+//Task-2
+let str1=`Hello ${name},
+how are you???
+Happy coding...`;
+console.log(str1);
